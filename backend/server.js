f6767a79ee244095640b08d0ee4f81853c322373
NikitaS <niks.nikitas05@gmail.com>
2026-05-26 18:14:17 +0530

    updated vercel url

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
-  'https://sharetogive.vercel.app/',
+  'https://sharetogive.vercel.app',
   'https://kindnestplatformproject.vercel.app',
   process.env.FRONTEND_URL,
 ].filter(Boolean);
